389958817ac67071b931337112ca347984aa219d
hemalatha <avhemavinoth@gmail.com>
2016-08-29 16:12:40 +0530

    Update and rename cn.java to cn1.java

---
+++ cn1.java
@@ -0,0 +1,17 @@
+class cn1 {
+public static void main(String args[]){
+	int e;
+	int n=e;
+	if(n=='a'||'e'||'i'||'o'||'u')
+	{
+		
+		System.out.println("vowels");
+	}
+	else
+	{
+		char[] consont;
+		System.out.println("consont");
+	}
+
+}
+}
